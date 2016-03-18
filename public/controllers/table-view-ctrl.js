4beaa0bae82e7e9fbe3ff48ddb262979f28c0bfa
NatalliaKiraikova <n.kozlova@gmail.com>
2016-03-18 17:14:43 +0300

    Ui-router was added, there is possibility to view data

---
+++ public/controllers/table-view-ctrl.js
@@ -0,0 +1,23 @@
+(function () {
+    'use strict';
+
+    angular.module('app')
+        .controller('TableViewController', TableViewController);
+
+    /** @ngInject */
+    function TableViewController($scope, items) {
+        if (items.data && items.data.length) {
+            $scope.columns = getColumnsArrayFromObject(items.data[0]);
+        }
+        $scope.items = items.data;
+    };
+
+    function getColumnsArrayFromObject(object) {
+        var columnArray = [];
+        for (var key in object) {
+            columnArray.push(key);
+        }
+        return columnArray;
+    }
+})();
+
--- public/routing.js
+++ public/routing.js
@@ -5,15 +5,62 @@
         .config(appConfig);
 
     /** @ngInject */
-    function appConfig($routeProvider) {
-        $routeProvider.when('/violationsview', {
-            templateUrl: 'tpl/violations-table.html',
-            controller: 'ViolationsCtrl'
-        }).when('/database', {
-            templateUrl: 'tpl/violations-table.html',
-            controller: 'ViolationsCtrl'
-        }).otherwise({
-            redirectTo: '/'
+    function appConfig($stateProvider) {
+
+        $stateProvider.state('home', {
+            templateUrl: 'tpl/main-view.html',
+            url: '/home',
+            pageTitle: 'Home Page'
+        }).state('violations', {
+            parent: 'home',
+            url: '/violations',
+            templateUrl: 'tpl/table-view.html',
+            controller: 'TableViewController',
+            resolve: {
+                items: function ($http) {
+                    return $http({method: 'GET', url: 'http://localhost:3000/violations'});
+                }
+            },
+            pageTitle: 'Violations Page'
+        }).state('database', {
+                parent: 'home',
+                url: '/database',
+                templateUrl: 'tpl/table-view.html',
+                controller: 'TableViewController',
+                resolve: {
+                    items: function ($http) {
+                        return $http({method: 'GET', url: 'http://localhost:3000/database'});
+                    }
+                },
+                pageTitle: 'Database Page'
+            }
+        ).state('violations-from-database-by-period', {
+                parent: 'home',
+                url: '/violations-from-database-by-period',
+                templateUrl: 'tpl/table-view.html',
+                controller: 'TableViewController',
+                resolve: {
+                    items: function ($http) {
+                        return $http({
+                            method: 'GET',
+                            url: 'http://localhost:3000/violations-from-database-by-period/?startDate=1456402158936&endDate=1458216558936'
+                        });
+                    }
+                }
+            }
+        ).state('names-from-violations-by-period', {
+            parent: 'home',
+            url: '/names-from-violations-by-period',
+            templateUrl: 'tpl/table-view.html',
+            controller: 'TableViewController',
+            resolve: {
+                items: function ($http) {
+                    return $http({
+                        method: 'GET',
+                        url: 'http://localhost:3000/names-from-violations-by-period/?startDate=1456402158936&endDate=1458216558936'
+                    });
+                }
+            }
         });
 
     }
