ffd2a91cf0eb69db391f9e0c88b0674d51bdf4b2
NatalliaKiraikova <n.kozlova@gmail.com>
2016-03-22 17:44:16 +0300

    suggestions from database processing was changed.

--- public/directives/search-input-drv.js
+++ public/directives/search-input-drv.js
@@ -15,9 +15,23 @@
     function SearchInputDirectiveController($scope, SuggestionsService) {
         $scope.searchStr = '';
 
+        $scope.$watch('searchStr', function (newValue, oldValue) {
+            var sLength = String(newValue).length;
+            if (sLength > 2) {
+                SuggestionsService.getSuggestions($scope.searchStr).then(function (res) {
+                    $scope.suggestions = res.data;
+                }, function (reason) {
+                    //error
+                    $scope.suggestions = [];
+                });
+            } else {
+                $scope.suggestions = [];
+            }
+        });
+
         $scope.search = function () {
+            //TODO works only for test
             SuggestionsService.getSuggestions($scope.searchStr).then(function (res) {
-                //TODO works only for test
                 $scope.suggestions = res.data;
             });
         };
