3919331baf2ebf14951be53a3b96c375f409b04d
NatalliaKiraikova <n.kozlova@gmail.com>
2016-03-23 17:13:32 +0300

    Suggestions list directive is shown like dropdown. Functionality was replaced to this directory with isolated scope.

--- public/directives/search-input-drv.js
+++ public/directives/search-input-drv.js
@@ -12,40 +12,17 @@
         });
 
     /** @ngInject */
-    function SearchInputDirectiveController($scope, SuggestionsService) {
+    function SearchInputDirectiveController($scope) {
         $scope.searchStr = '';
-
-        $scope.status = {
-            isopen: false
-        };
-
-        $scope.$watch('searchStr', function (newValue, oldValue) {
-            var sLength = String(newValue).length;
-            if (sLength > 2) {
-                SuggestionsService.getSuggestions($scope.searchStr).then(function (res) {
-                    $scope.suggestions = res.data;
-                    $scope.status.isopen = true;
-                }, function (reason) {
-                    //error
-                    $scope.suggestions = [];
-                    $scope.status.isopen = false;
-                });
-            } else {
-                $scope.suggestions = [];
-                $scope.status.isopen = false;
-            }
-        });
 
         $scope.search = function () {
             //TODO works only for test
-            SuggestionsService.getSuggestions($scope.searchStr).then(function (res) {
+            /*SuggestionsService.getSuggestions($scope.searchStr).then(function (res) {
                 $scope.suggestions = res.data;
-            });
+            });*/
         };
 
-        function toggleDropdown() {
-            $scope.status.isopen = !$scope.status.isopen;
-        }
+
     }
 
 })();
--- public/directives/suggestions-list-drv.js
+++ public/directives/suggestions-list-drv.js
@@ -7,11 +7,37 @@
             return {
                 restrict: 'E',
                 templateUrl: "directives/tpl/suggestions-list-tpl.html",
+                scope: {
+                    labelledby: "@",
+                    searchStr: "="
+                },
                 controller: SuggestionsListDirectiveController
             };
         });
     /** @ngInject */
-    function SuggestionsListDirectiveController($scope) {
+    function SuggestionsListDirectiveController($scope, SuggestionsService) {
+
+        $scope.status = {
+            isopen: false
+        };
+
+        $scope.$watch('searchStr', function (newValue, oldValue) {
+            var sLength = String(newValue).length;
+            if (sLength > 2) {
+                SuggestionsService.getSuggestions($scope.searchStr).then(function (res) {
+                    $scope.suggestions = res.data;
+                    $scope.status.isopen = true;
+                }, function (reason) {
+                    //error
+                    $scope.suggestions = [];
+                    $scope.status.isopen = false;
+                });
+            } else {
+                $scope.suggestions = [];
+                $scope.status.isopen = false;
+            }
+        });
+
         $scope.listClick = function (item) {
             alert("Hello! I am an " + item + "!!");
         };
